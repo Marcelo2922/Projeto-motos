33cbf7f480591cd416d90e2863f48435de497dc7
MARCELO AUGUSTO DE OLIVEIRA RODRIGUES <marcelo_rodrigues155@estudante.sesisenai.org.br>
2026-04-28 20:21:50 -0300

    Criação do projeto

--- src/paginas/ListaProdutos/ItemProduto.jsx
+++ src/paginas/ListaProdutos/ItemProduto.jsx
@@ -2,20 +2,33 @@ import "./ItemProduto.css";
 
 function ItemProduto({ produto }) {
   return (
-    <div className="item-produto__root">
-      <span>{produto.nome}</span>
-      <span>{produto.preco}</span>
+    <div className="card-moto">
+      <img
+        src={produto.imagem}
+        alt={produto.nome}
+        className="card-moto-img"
+      />
 
-      <div className="item-produto__cores">
-        {produto.cores.map((aquarela, contagem) => {
-          return (
-            <div
-              key={contagem}
-              className="item-produto__cor"
-              style={{ backgroundColor: aquarela }}
+      <div className="card-moto-info">
+        <h2>{produto.nome}</h2>
+
+        <p className="preco">
+          R$ {produto.preco.toLocaleString("pt-BR")}
+        </p>
+
+        <div className="cores">
+          {produto.cores.map((cor, i) => (
+            <span
+              key={i}
+              className="cor"
+              style={{ backgroundColor: cor }}
             />
-          );
-        })}
+          ))}
+        </div>
+
+        <button className="btn-detalhes">
+          Ver detalhes
+        </button>
       </div>
     </div>
   );
