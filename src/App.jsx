915c162c512cbd6d6cf038148d2100cabe2f1787
MARCELO AUGUSTO DE OLIVEIRA RODRIGUES <marcelo_rodrigues155@estudante.sesisenai.org.br>
2026-04-28 19:50:16 -0300

    Criação do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+// imports de bibliotecas externas, instaladas via npm
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// imports de arquivos de estilos (CSS)
+import "./App.css";
+// imports de componentes/paginas internas do projeto React (arquivos .jsx)
+import Cabecalho from "./componentes/Cabecalho/Cabecalho";
+import Rodape from "./componentes/Rodape/Rodape";
+// imports de páginas
+import CadastroCliente from "./paginas/CadastroCliente/CadastroCliente";
+import ListaClientes from "./paginas/ListaClientes/ListaClientes";
+import ListaProdutos from "./paginas/ListaProdutos/ListaProdutos";
+import ListaTarefas from "./paginas/ListaTarefas/ListaTarefas";
+import PaginaInicial from "./paginas/PaginaInicial/PaginaInicial";
+
+const roteador = createBrowserRouter([
+  {
+    path: "",
+    element: <PaginaInicial />,
+  },
+  {
+    path: "lista-produtos",
+    element: <ListaProdutos />,
+  },
+  {
+    path: "lista-tarefas",
+    element: <ListaTarefas />,
+  },
+  {
+    path: "lista-clientes",
+    element: <ListaClientes />,
+  },
+  {
+    path: "cadastro-cliente/:clienteId?", // o "?" torna o parâmetro opcional, ou seja, pode ser acessado tanto para criar um novo cliente (sem id) quanto para editar um cliente existente (com id)
+    element: <CadastroCliente />,
+  },
+  {
+    path: "*", //
+    element: <h3>Página não encontrada!!</h3>,
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <Cabecalho />
+      <RouterProvider router={roteador} />
+      <Rodape />
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/paginas/ListaProdutos/ListaProdutos.jsx
@@ -0,0 +1,22 @@
+const produtos = [
+  {
+    nome: "Honda CG 160",
+    preco: 14000,
+    cores: ["#000000", "#ff0000", "#ffffff"],
+  },
+  {
+    nome: "Yamaha Fazer 250",
+    preco: 19000,
+    cores: ["#0033cc", "#000000", "#cccccc"],
+  },
+  {
+    nome: "Kawasaki Ninja 400",
+    preco: 33000,
+    cores: ["#00ff00", "#000000"],
+  },
+  {
+    nome: "BMW G 310 R",
+    preco: 35000,
+    cores: ["#ffffff", "#0000ff", "#ff0000"],
+  },
+];
