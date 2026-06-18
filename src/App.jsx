a2f913e69efe7afdfc7cc93fe25133fb51b3a481
MARCELO AUGUSTO DE OLIVEIRA RODRIGUES <marcelo_rodrigues155@estudante.sesisenai.org.br>
2026-06-17 21:11:33 -0300

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,39 @@ import "./App.css";
 
 import Cabecalho from "./componentes/Cabecalho/Cabecalho";
 import Rodape from "./componentes/Rodape/Rodape";
+import ValidarAutenticacao from "./componentes/ValidarAutenticacao/ValidarAutenticacao";
+import AppContextProvider from "./contexto/AppContext";
+
 
 import ListaProdutos from "./paginas/ListaProdutos/ListaProdutos";
 import PaginaInicial from "./paginas/PaginaInicial/PaginaInicial";
 import CadastroMoto from "./paginas/CadastroMoto/CadastroMoto";
 import Estoque from "./paginas/Estoque/Estoque";
+import Login from "./paginas/Login/Login";
+import NovoUsuario from "./paginas/NovoUsuario/NovoUsuario";
+import PerfilUsuario from "./paginas/PerfilUsuario/PerfilUsuario";
 
 
 const roteador = createBrowserRouter([
   {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "novo-usuario",
+    element: <NovoUsuario />,
+  },
+  {
+    path: "",
+    element: <ValidarAutenticacao />,
+    children: [
+  {
     path: "/",
     element: <PaginaInicial />,
+  },
+  {
+   path: "meu-perfil",
+   element: <PerfilUsuario />,
   },
   {
     path: "/lista-de-motos",
@@ -30,19 +52,24 @@ const roteador = createBrowserRouter([
     path: "/cadastro-moto/:motoId?",
     element: <CadastroMoto />,
   },
+],
+  },
   {
     path: "*",
     element: <h3>Página não encontrada!!</h3>,
   },
+
 ]);
 
 function App() {
   return (
     <>
-      <Cabecalho />
-      <RouterProvider router={roteador} />
-      <Rodape />
-      <ToastContainer />
+      <AppContextProvider>
+        <Cabecalho />
+        <RouterProvider router={roteador} />
+        <Rodape />
+        <ToastContainer />
+      </AppContextProvider>
     </>
   );
 }
